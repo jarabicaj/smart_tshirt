1574c0e32b7e3adc10e718c7739b6dc44500b37c
Unknown <jarabica.j@gmail.com>
2017-10-28 18:10:53 +0200

    add comments to fillData

--- src/sockets/dev/fillData.js
+++ src/sockets/dev/fillData.js
@@ -1,36 +1,36 @@
 const mysql = require("mysql") // library - mysql node client
 
-const connection = mysql.createConnection({
+const connection = mysql.createConnection({ // definovanie pripojenia k databaze
   host     : 'localhost',
   user     : 'root',
   password : '',
   database : 'test',
 });
 
-connection.connect();
+connection.connect(); // pokus o pripojenie k databaze
 
-const users = [
-  {
-    firstname: "Jozef",
+const users = [ // pole
+  {                           // objekt 1
+    firstname: "Jozef",       // nazov atributu = hodnota atributu
     lastname: "Džejo",
     age: 32,
   },
-  {
+  {                             // objekt 2
     firstname: "Iwka",
     lastname: "Modelka",
     age: 19,
   },
-  {
+  {                                 // objekt 3
     firstname: "Natalka",
     lastname: "RadaVinkova",
     age: 23,
   },
 ]
 
-users.forEach(({firstname, lastname, age}) => {
+users.forEach(({firstname, lastname, age}) => {   // pre kazdeho usera sa vykona funkcia a vytiahnem do premennych alebo bez vytiahnutia ale dalej user.firstname atd
   connection.query(`INSERT INTO table_1 (firstname, lastname, age) VALUES ('${firstname}', '${lastname}', '${age}')`, (error, results, fields) => {
     if (error) throw error;
-    console.log(results.insertId);
+    console.log(results.insertId); // vypise do konzoly id
   })
 })
 
