6e2146de4b826dead872a6dac3ef136f3025caf1
VladimirJarabica <vladojarabica@gmail.com>
2018-03-31 18:04:19 +0200

    delete old stuff

--- src/server/database/models/user/model.js
+++ src/server/database/models/user/model.js
@@ -7,5 +7,3 @@ const userSchema = mongoose.Schema({
 const User = mongoose.model("User", userSchema);
 
 export default User;
-
-
--- src/sockets/client/scenes/historyData/index.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-class HistoryData extends Component {
-  render() {
-    return <div>History Data</div>
-  }
-}
-
-export default HistoryData
